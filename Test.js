f21329be768ac0c921b53fd89f8006bd545cd82c
BenWng <wnghan@gmail.com>
2016-09-20 10:57:23 +0800

    The test for imaginaryebay

--- Test.js
+++ Test.js
@@ -5,21 +5,44 @@
 
 var request=require ('request');
 var q=require('q');
+var j=request.jar();
 
 
-request.post('http://localhost:8080/login',
-    {form:{username:'butters@gmail.com',password:'Stotch'}},
-    function(err,httpResponse,body){
-        var rawCookie=(httpResponse.headers['set-cookie'])[0];
-        
+
+
+var cookiePromise=q.defer();
+getCookie(cookiePromise);
+
+
+function getCookie(promise){
+    request.post('http://localhost:8080/login',
+        {form:{username:'butters@gmail.com',password:'Stotch'}},
+        function(err,httpResponse,body){
+            var rawCookie=(httpResponse.headers['set-cookie'])[0];
+            promise.resolve(rawCookie);
+        }
+    )
+}
+
+
+cookiePromise.promise.then(function(rawCookie){
+        var cookie=request.cookie(rawCookie);
+        var url='http://localhost:8080/user';
+        j.setCookie(cookie,url);
+        request({url:url, jar: j},function(err,httpResponse,body){
+        console.log(body);
+        })
     }
 )
 
-var j=request.jar();
-var cookie=request.cookie(rawCookie);
-var url='http://localhost:8080/user';
-j.setCookie(cookie,url);
-request({url:url, jar: j},function(err,httpResponse,body){
-    console.log(body);
-})
 
+
+cookiePromise.promise.then(function(rawCookie){
+        var cookie=request.cookie(rawCookie);
+        var url='http://localhost:8080/user/item/1';
+        j.setCookie(cookie,url);
+        request({url:url, jar: j},function(err,httpResponse,body){
+        console.log(body);
+        })
+    }
+)
